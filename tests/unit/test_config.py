93bfcdb6b18a642f04fa144cec5f12053bbc8493
Andreas Maier <maiera@de.ibm.com>
2024-05-10 12:23:49 +0200

    Added missing .coveragerc file; Added TESTOPTS; Added first unit testcase
    Details:
    
    * Added the missing .coveragerc file that was referenced in the 'make test'
      commands, but not present.
    
    * Added support for a TESTOPTS env var that sets pytest options.
    
    * Added a first simple unit test case: tests/unt/test_config.py.
    
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

---
+++ tests/unit/test_config.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""Unit tests for zhmc_log_forwarder.Config class"""
+
+import os
+import sys
+import uuid
+
+import pytest
+import zhmcclient
+
+from zhmc_log_forwarder import zhmc_log_forwarder
+
+# Prefix for any created resources
+TEST_PREFIX = 'zhmc_log_forwarder_config'
+
+
+TESTCASES_CONFIG_LOAD = [
+    #
+    # desc (str): Testcase description
+    # config_file (str): Content of config file to be loaded.
+    # exp_parms (dict): Expected config file parameters, if success.
+    # exp_exc (exc class): Expected exception class, if failure.
+    (
+        "Empty config file",
+        "",
+        {},
+        zhmc_log_forwarder.UserError
+    ),
+    (
+        "Config file with minimal properties",
+        """
+hmc_host: 10.11.12.13
+hmc_user: user1
+hmc_password: pw1
+""",
+        {
+            "hmc_host": "10.11.12.13",
+            "hmc_user": "user1",
+            "hmc_password": "pw1",
+            # Default items:
+            'check_data': {'functional_users': [], 'imgmt_subnet': None},
+            'forwardings': [],
+            'future': False,
+            'label': None,
+            'log_message_file': None,
+            'selflog_dest': 'stdout',
+            'selflog_format': '%(levelname)s: %(message)s',
+            'selflog_time_format': '%Y-%m-%d %H:%M:%S.%f%z',
+        },
+        None
+    ),
+]
+
+@pytest.mark.parametrize(
+    "desc, config_file, exp_parms, exp_exc", TESTCASES_CONFIG_LOAD
+)
+def test_config_load(desc, config_file, exp_parms, exp_exc):
+    """Tests if some generic file is correctly parsed."""
+
+    config_filename = "{}_{}.yaml".format(TEST_PREFIX, uuid.uuid4().hex)
+    with open(config_filename, "w+", encoding='utf-8') as cf:
+        cf.write(config_file)
+
+    try:
+        config = zhmc_log_forwarder.Config()
+        if exp_exc:
+            with pytest.raises(exp_exc):
+
+                config.load_config_file(config_filename)
+
+        else:
+
+            config.load_config_file(config_filename)
+
+            assert config.parms == exp_parms
+    finally:
+        os.remove(config_filename)
