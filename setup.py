8f30baa8b93b0d698eac2de8412a38171f516f94
Andreas Maier <maiera@de.ibm.com>
2024-06-13 11:26:09 +0200

    Added Trove classifiers for Python versions up to 3.12
    Signed-off-by: Andreas Maier <maiera@de.ibm.com>

--- setup.py
+++ setup.py
@@ -134,6 +134,10 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Systems Administration',
     ]
